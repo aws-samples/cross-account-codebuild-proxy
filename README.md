26fcadb24bd6b1d2700cf8cb0d2e695db99ced0f
Richard Milner-Watts <milnerwa@amazon.co.uk>
2021-10-27 10:22:11 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,11 +1,28 @@
-## My Project
+## Cross Account CodeBuild Proxy
 
-TODO: Fill this README out!
+### Summary
 
-Be sure to:
+AWS CodeBuild provides an easy mechanism to execute commands from a Linux shell with a configurable IAM principal. This enables a variety of operational tasks, with environment variables used at runtime to control behavior.
 
-* Change the title in this README
-* Edit your repository description on GitHub
+This sample code provides an example of how AWS CodeBuild projects can be orchestrated across account boundaries, for example a central account could execute AWS CodeBuild projects across every account in an AWS Organization.
+
+### Features
+
+The included AWS Lambda function provides a method to start AWS CodeBuild projects, along with a method to check the status of a particular AWS CodeBuild execution ID.
+
+This may be combined with the sample AWS Step Function state machine to implement a workflow which starts an AWS CodeBuild project in a remote account with environment variable overrides, before regularly polling the execution status and capturing the final result. 
+
+### Solution Architecture
+
+The solution architecture is shown below:
+
+![Solution Architecture](https://github.com/aws-samples/cross-account-codebuild-proxy/blob/main/docs/Solution-Architecture-Diagram.png)
+
+The solution is delivered via two AWS CloudFormation templates. 
+
+The template *codebuild_lambda_proxy_template.yaml* will deploy the AWS Lambda function and the sample AWS Step Function state machine, along with supporting IAM roles. This should be deployed first to ensure the IAM principals may be used as trusted entities. 
+
+The second template *sample_target_codebuild_template.yaml* will deploy a sample AWS CodeBuild project which may be used to test the sample. It will also deploy two IAM roles, one for AWS CodeBuild and one which is to be assumed from the AWS account which contains the AWS Lambda proxy. 
 
 ## Security
 
---
+++ src/cross_account_codebuild_proxy.py
@@ -0,0 +1,121 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import boto3
+import logging
+import sys
+import traceback
+import os
+import json
+from botocore.exceptions import ClientError
+
+sts_client = boto3.client('sts')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def log_exception(exception_type, exception_value, exception_traceback):
+    """Function to create a JSON object containing exception details, which can then be logged as one line to the logger."""
+    traceback_string = traceback.format_exception(exception_type, exception_value, exception_traceback)
+    err_msg = json.dumps({"errorType": exception_type.__name__, "errorMessage": str(exception_value), "stackTrace": traceback_string})
+    logger.error(err_msg)
+
+def assume_role(role_arn: str):
+    """Wrapper function to assume an IAM Role."""
+    try:
+        logger.info(f"Assuming Role: {role_arn}")
+        assumedRole = sts_client.assume_role(
+            RoleArn=role_arn,
+            RoleSessionName='cross_account_role'
+        )
+    except:
+        log_exception(*sys.exc_info())    
+        raise RuntimeError(f"Could not assume role: {role_arn}")
+    return boto3.Session(
+        aws_access_key_id=assumedRole['Credentials']['AccessKeyId'],
+        aws_secret_access_key=assumedRole['Credentials']['SecretAccessKey'],
+        aws_session_token=assumedRole['Credentials']['SessionToken'])
+
+
+def start_codebuild_project(codebuild_session_object, codebuild_project, codebuild_envvars):
+    """Start CodeBuild Project with environment variable overrides"""
+    try:
+        codebuild_response = codebuild_session_object.start_build(
+                            projectName=codebuild_project,
+                            environmentVariablesOverride=codebuild_envvars
+                        )
+    except:
+        log_exception(*sys.exc_info())    
+        raise RuntimeError(f"Could not start CodeBuild Project: {codebuild_project}")
+
+    logger.info(f"Started CodeBuild Job: {codebuild_response['build']['id']}")
+    return {
+        'codeBuildJobId': codebuild_response['build']['id']
+    }  
+
+def check_codebuild_status(codebuild_session_object, codebuild_job_id):
+    """Check CodeBuild job status"""
+    try:
+        codebuild_response = codebuild_session_object.batch_get_builds(
+                                ids=[codebuild_job_id]
+                            )
+    except:
+        log_exception(*sys.exc_info())    
+        raise RuntimeError(f"Exception checking job status: {codebuild_job_id}")
+    return {
+        'jobStatus': codebuild_response['builds'][0]['buildStatus']
+    }
+
+def start_build_handler(event):
+    """Handler for start CodeBuild workflow"""
+    if not event['roleArn']:
+        raise Exception("Event did not include the roleArn")
+    if not event['codeBuildProject']:
+        raise Exception("Event did not include the target CodeBuild Project")
+    if not event['environmentVariables']:
+        event['environmentVariables'] = []
+
+    boto3_session = assume_role(event['roleArn'])
+    codebuild_client = boto3_session.client('codebuild')
+
+    codebuild_start = start_codebuild_project(codebuild_client, event['codeBuildProject'], event['environmentVariables'])
+
+    # append CodeBuild Job Id to the supplied event
+    event.update({"CodeBuildJobStatus": "IN_PROGRESS"})
+    event.update({"CodeBuildJobId": codebuild_start['codeBuildJobId']})
+    return event
+
+def check_build_status_handler(event):
+    """Handler for checking CodeBuild job status"""
+    if not event['roleArn']:
+        raise Exception("Event did not include the roleArn")
+    if not event['jobId']:
+        raise Exception("Event did not include the CodeBuild ID to check")
+
+    boto3_session = assume_role(event['roleArn'])
+    codebuild_client = boto3_session.client('codebuild')
+
+    codebuild_status = check_codebuild_status(codebuild_client, event['jobId'])
+
+    # append CodeBuild Job Id to the supplied event
+    event.update({"CodeBuildJobStatus": codebuild_status['jobStatus']})
+    event.update({"CodeBuildJobId": event['jobId']})
+    return event
+
+def lambda_handler(event, context):
+    if event['invocationType'] == "START_BUILD":
+        return start_build_handler(event)
+    if event['invocationType'] == "CHECK_STATUS":
+        return check_build_status_handler(event)
